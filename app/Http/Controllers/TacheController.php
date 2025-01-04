76cacb975b7af5ff65ff266283afe90b0e5bdeac
Fayecodeur <fayecodeur99@gmail.com>
2025-01-04 16:11:18 +0000

    ajout des methodes pour le crud

--- app/Http/Controllers/TacheController.php
+++ app/Http/Controllers/TacheController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SaveTaskRequest;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TacheController extends Controller
@@ -11,7 +13,7 @@ class TacheController extends Controller
      */
     public function index()
     {
-        //
+        return view("edit", ['tasks' => Task::all()]);
     }
 
     /**
@@ -19,40 +21,42 @@ class TacheController extends Controller
      */
     public function create()
     {
-        //
+        return view('create');
     }
-
-
 
     /**
-     * Display the specified resource.
+     * Store a newly created resource in storage.
      */
-    public function show(string $id)
+    public function store(SaveTaskRequest $request)
     {
-        //
+        Task::create($request->validated());
+        return redirect()->route('index')->with('success', 'Tâche ajoutée avec succès!');
     }
+
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Task $task)
     {
-        //
+        return view('create', compact('task'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(SaveTaskRequest $request, Task $task)
     {
-        //
+        $task->update($request->validated());
+        return redirect()->route('index')->with('success', 'Tâche modifiée avec succès!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function delete(string $id)
+    public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return redirect()->route('index')->with('success', 'Tâche supprimée avec succès!');
     }
 }
