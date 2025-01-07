72fc196650a47ba7889e065a5b538379c8a64421
Fayecodeur <fayecodeur99@gmail.com>
2025-01-07 13:58:56 +0000

    mise en place du formulaire de connexion

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,28 @@
-<div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
+@extends('layouts.app')
+
+@section('title', "Connexion")
+
+@section('content')
+<div class="d-flex align-items-center justify-content-center vh-100">
+    <div class="shadow-lg p-5 rounded bg-white" style="width: 100%; max-width: 500px;">
+        <h3 class="mb-4 text-center">Connexion</h3>
+        <form method="POST" class="py-4" action="#">
+            @csrf
+            <!-- Champ Email -->
+            <div class="mb-4">
+                <label for="email" class="form-label">Adresse email</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Entrez votre adresse email" >
+            </div>
+            
+            <!-- Champ Mot de Passe -->
+            <div class="mb-4">
+                <label for="password" class="form-label">Mot de passe</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Entrez votre mot de passe" >
+            </div>
+
+            <!-- Bouton Connexion -->
+            <button type="submit" class="btn btn-primary w-100">Se connecter</button>
+        </form>
+    </div>
 </div>
+@endsection
